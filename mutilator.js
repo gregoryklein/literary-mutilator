af2309e05d368600acb84599a748a653a40a2f74
Greg Klein <gregkleinwrites@gmail.com>
2018-11-12 19:41:32 -0700

    added a function to append BORK! to last p tag. will try to make it react to a keyboard event later.

--- mutilator.js
+++ mutilator.js
@@ -1,9 +1,17 @@
+//changes text color
 function textColor() {
 	document.getElementById("text").style.color = "#ff9d00";
 	console.log("hi");
 }
 
-
+//changes font
 function changeFont() {
 	document.getElementById("text").style.font = "italic bold 16px sans-serif";
 }
+
+//will try to change this to a keyboard event. currently adds BORK to end of p tag
+function newBork() {
+		var oldtext = document.getElementById("addBork");
+
+		oldtext.textContent += "BORK! ";
+	}
