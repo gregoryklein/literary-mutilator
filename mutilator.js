03a7eb5318d854b0efb2d7da4eb773f5ae852206
Greg Klein <gregkleinwrites@gmail.com>
2018-11-12 18:57:21 -0700

    added font change button

--- mutilator.js
+++ mutilator.js
@@ -1,9 +1,9 @@
 function textColor() {
-	document.getElementById("text").style.color = "red";
+	document.getElementById("text").style.color = "#ff9d00";
 	console.log("hi");
 }
 
 
 function changeFont() {
-
+	document.getElementById("text").style.font = "italic bold 16px sans-serif";
 }
