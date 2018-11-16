09c21985179dc87a6198d0e210529964c9352d0d
Greg Klein <gregkleinwrites@gmail.com>
2018-11-15 19:47:18 -0700

    got ROT13 working!

--- mutilator.js
+++ mutilator.js
@@ -26,3 +26,12 @@ document.addEventListener("keydown", function newBork(event) {
 		oldtext.textContent += "BORK! ";
 	}
 });
+
+function rot(s) {
+	return s.replace(/[a-zA-Z]/g, function (c) {
+		return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+	});
+}
+function update() {
+	document.getElementById('output').value = rot(document.getElementById('input').value);
+}
